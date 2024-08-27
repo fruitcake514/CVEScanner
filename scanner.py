686840ab8d62a9252484cc409aafbff8b7f4ff5a
Nathaniel Smith <146392034+fruitcake514@users.noreply.github.com>
2024-08-27 15:13:57 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,8 +1,9 @@
-# scanner.py
+import os
 import nmap
 
 def scan_network():
+    ip_range = os.getenv('IP_RANGE', '192.168.1.0/24')
     nm = nmap.PortScanner()
-    nm.scan(hosts='192.168.1.0/24', arguments='-sP')
+    nm.scan(hosts=ip_range, arguments='-sP')
     hosts = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
     return hosts
